fd8c9832c2552b7ccfd7ef9129b09b01a33e906a
papazhora <4156464@gmail.com>
2018-04-05 17:53:02 +0300

    2

--- gulpfile.js
+++ gulpfile.js
@@ -85,3 +85,26 @@ gulp.task('watch', ['styles', 'js', 'browser-sync'], function() {
 });
 
 gulp.task('default', ['watch']);
+
+gulp.task('clean', function() {
+    return del.sync('dist');
+});
+
+gulp.task('build', ['clean', 'img', 'styles', 'js'], function() {
+    var buildCss = gulp.src([
+        'app/css/main.css',
+        'app/css/libs.min.css',
+
+    ])
+    .pipe(gulp.dest('dist/css'));
+
+    var buildFonts = gulp.src('app/fonts/**/*')
+
+    .pipe(gulp.dest('dist/fonts'));
+
+    var buildJs = gulp.src('app/js/**/*')
+    .pipe(gulp.dest('dist/js'));
+
+    var buildHtml = gulp.src('app/*.html')
+    .pipe(gulp.dest('dist'));
+});
